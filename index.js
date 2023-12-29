4c060541d0ec8eb6c498213a448957c975ee2f16
flaviodrummond <flaviosax20@gmail.com>
2023-12-29 12:08:04 -0300

    Conectando o back-end com o Front-end, utilizando o cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3001
-app.use(express.json()) // Avisando o express que eu quero usar como padrão o Json
-import cors from "cors"
+const cors = require('cors')
 const uuid = require('uuid') // Criando uma variável para armazenar a biblioteca
 
+app.use(express.json()) // Avisando o express que eu quero usar como padrão o Json
 app.use(cors())
 
 
